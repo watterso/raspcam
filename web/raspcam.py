8b88f3608de7321635b67aef1522f267e6713986
James Watterson <jameslwatterson@gmail.com>
2014-12-19 14:52:20 -0500

    added light box. Basic Styling. timestamp parsing from image files

--- web/raspcam.py
+++ web/raspcam.py
@@ -1,15 +1,24 @@
+import os
+from time import strftime, strptime
+
 from flask import Flask, request, session, g, redirect, url_for, \
 		     abort, render_template, flash
 
-MEDIA_DIR = '../media'
+MEDIA_DIR = 'static/frames'
+FILE_TIME = '%Y%m%d_%H%M%S.jpg'
+DISP_TIME = '%B %d, %Y at %H:%M:%S'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.debug=True
 
 @app.route('/')
-def show_videos():
-  return render_template('show_videos.html', videos=[])
+def show_frames():
+
+  paths = [{'file':url_for('static', filename='frames/'+x), 'title':strftime(DISP_TIME, strptime(x,FILE_TIME))} for x in os.listdir(MEDIA_DIR)]
+  list.sort(paths, key=lambda pic: pic['file'], reverse=True)
+  most_recent = paths.pop(0) if len(paths) > 0 else None
+  return render_template('show_frames.html', most_recent=most_recent, frames=paths)
 
 if __name__ == '__main__':
     app.run()
