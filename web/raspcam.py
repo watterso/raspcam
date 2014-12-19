108f3a51d6fe1662c4d56bdfb30dc8d31fdc6bb4
James Watterson <jameslwatterson@gmail.com>
2014-12-19 16:19:31 -0500

    new layout, clicking images on the right now changes main image

--- web/raspcam.py
+++ web/raspcam.py
@@ -14,10 +14,9 @@ app.debug=True
 
 @app.route('/')
 def show_frames():
-
   paths = [{'file':url_for('static', filename='frames/'+x), 'title':strftime(DISP_TIME, strptime(x,FILE_TIME))} for x in os.listdir(MEDIA_DIR)]
   list.sort(paths, key=lambda pic: pic['file'], reverse=True)
-  most_recent = paths.pop(0) if len(paths) > 0 else None
+  most_recent = paths[0] if len(paths) > 0 else None
   return render_template('show_frames.html', most_recent=most_recent, frames=paths)
 
 if __name__ == '__main__':
